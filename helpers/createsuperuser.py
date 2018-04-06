8105b5100e45c1f4c6cc4b89ed3d6b82c8318431
gonczor <wiktor.gonczaronek@gmail.com>
2018-04-06 23:37:03 +0200

    Added password hashing

---
+++ helpers/createsuperuser.py
@@ -0,0 +1,32 @@
+from sys import exit
+from getpass import getpass
+
+from db import db
+from db.models import User
+from main import create_app
+from utils.hash import hash_password
+
+
+def execute():
+    create_app()
+
+    username = input('Give username: ')
+    password = getpass('Give password: ')
+    password = hash_password(password)
+    password_confirmation = getpass('Confirm password: ')
+    password_confirmation = hash_password(password_confirmation)
+
+    if password != password_confirmation:
+        print('Passwords do not match')
+        exit(1)
+
+    admin = User(username=username, password=password)
+    db.session.add(admin)
+    db.session.commit()
+
+    # Check
+    user = User.query.filter(User.username == username).first()
+    if user:
+        print('Successfully created user: {}'.format(user.username))
+    else:
+        print('User not created.')
--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from app.urls import register_urls as register_app_urls
 from api.urls import register_urls as register_api_urls
 from db import db
 
+from helpers import createsuperuser
+
 
 def create_app():
     app = Flask(__name__)
@@ -26,7 +28,9 @@ if __name__ == '__main__':
     if 'test' in sys.argv:
         suite = unittest.defaultTestLoader.discover('tests')
         unittest.TextTestRunner().run(suite)
-    if 'createdb' in sys.argv:
+    elif 'createdb' in sys.argv:
         db.create_all()
+    elif 'createsuperuser' in sys.argv:
+        createsuperuser.execute()
     else:
         rest_app.run(debug=True)
