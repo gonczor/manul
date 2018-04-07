5bb28a218b23e12298f4e0483b6f2ea7980ca361
gonczor <wiktor.gonczaronek@gmail.com>
2018-04-07 22:04:21 +0200

    Added dummy templates for admin

--- app/urls.py
+++ app/urls.py
@@ -3,3 +3,4 @@ from app.views import hello_world
 
 def register_urls(app):
     app.add_url_rule('/', 'hello_world', hello_world)
+    app.add_url_rule('')
--- app/views.py
+++ app/views.py
@@ -1,5 +1,15 @@
 from flask import render_template
+from flask_login import login_required
 
 
 def hello_world():
     return render_template('welcome_page.html', name_from_python='\"Hello, world!\"')
+
+
+def login():
+    return render_template('login.html')
+
+
+@login_required
+def admin_panel():
+    return render_template('admin_panel.html')
